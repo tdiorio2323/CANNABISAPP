6765bfb56e90b704508010150e51ba49f94fa07f
Your Name <you@example.com>
2025-09-03 15:18:38 -0500

    fix: add API proxy for development

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      '/api': {
+        target: 'https://cabanagrp.com',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
   },
   plugins: [
     react(),
